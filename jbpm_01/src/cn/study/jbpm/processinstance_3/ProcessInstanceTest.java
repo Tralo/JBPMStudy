29921dfb8c9a3bf5fb37b789b3ce0b972519e950
Tralo <13580548169@163.com>
2017-02-09 14:43:27 +0800

    流程实例流转以及直接中止流程操作

--- jbpm_01/src/cn/study/jbpm/processinstance_3/ProcessInstanceTest.java
+++ jbpm_01/src/cn/study/jbpm/processinstance_3/ProcessInstanceTest.java
@@ -7,20 +7,43 @@ import org.jbpm.api.ProcessInstance;
 import org.junit.Test;
 
 public class ProcessInstanceTest {
-	
+
 	@Test
-	//启动流程实例
-	public void demo1(){
+	// 启动流程实例
+	public void demo1() {
 		// 步骤一: 获取流程引擎
 		ProcessEngine processEngine = new Configuration().buildProcessEngine();
-		// 步骤二: 
+		// 步骤二: 获得对应 service 对象
 		ExecutionService executionService = processEngine.getExecutionService();
-		
+
 		// 步骤三: 启动流程实例
-//		executionService.startProcessInstanceById("holiday-1");//ID 是唯一的
-		
+		// executionService.startProcessInstanceById("holiday-1");//ID 是唯一的
+
 		// 根据 pdKey 启动（jbpm4_deployprop存在key属性）
 		executionService.startProcessInstanceByKey("holiday");// key 可以相同的
+
+	}
+
+	@Test
+	// 测试流程实例 向后流转
+	public void demo2() {
+		// 步骤一: 获取流程引擎
+		ProcessEngine processEngine = new Configuration().buildProcessEngine();
+		// 步骤二: 获得对应 service 对象
+		ExecutionService executionService = processEngine.getExecutionService();
+		// 步骤三: 根据实例id，向后流转
+		// executionService.signalExecutionById("holiday.50001","to 部门经理审批");
+		executionService.signalExecutionById("holiday.50001", "to end1");
+	}
+
+	@Test
+	public void demo3() {
+		// 步骤一: 获取流程引擎
+		ProcessEngine processEngine = new Configuration().buildProcessEngine();
+		// 步骤二: 获得对应 service 对象
+		ExecutionService executionService = processEngine.getExecutionService();
+		// 步骤三: 直接中止流程
+		executionService.endProcessInstance("holiday.60001", ProcessInstance.STATE_ENDED);
 		
 	}
 
