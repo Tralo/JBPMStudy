78b3cf2dbba113c49501225cb3d421049336bcd8
Tralo <13580548169@163.com>
2017-02-09 15:21:21 +0800

    查看个人任务列表，办理个人任务

--- jbpm_01/src/cn/study/jbpm/task_4/TaskTest.java
+++ jbpm_01/src/cn/study/jbpm/task_4/TaskTest.java
@@ -16,19 +16,47 @@ public class TaskTest {
 
 	@Test
 	// 查看某个流程实例的当前任务
-	public void demo1(){
+	public void demo1() {
 		// 1. 流程引擎
 		ProcessEngine processEngine = new Configuration().buildProcessEngine();
 		// 2. 获得对应 Service
 		TaskService taskService = processEngine.getTaskService();
-		
+
 		// 3. 查询流程实例的任务
 		TaskQuery taskQuery = taskService.createTaskQuery();
 		List<Task> list = taskQuery.executionId("holiday.30001").list();
-		for(Task task : list){
+		for (Task task : list) {
 			System.out.println("任务编号: " + task.getId());
 			System.out.println("任务名称: " + task.getName());
 		}
 	}
-	
+
+	@Test
+	// 查看我的个人任务
+	public void demo2() {
+		// 1. 流程引擎
+		ProcessEngine processEngine = new Configuration().buildProcessEngine();
+		// 2. 获得对应 Service
+		TaskService taskService = processEngine.getTaskService();
+		// 3. 查看个人任务
+//		List<Task> list = taskService.findPersonalTasks("员工");
+		List<Task> list = taskService.findPersonalTasks("部门经理");
+		for (Task task : list) {
+			System.out.println("任务编号: " + task.getId());
+			System.out.println("任务名称: " + task.getName());
+		}
+	}
+
+	@Test
+	// 办理个人任务
+	public void demo3() {
+		// 1. 流程引擎
+		ProcessEngine processEngine = new Configuration().buildProcessEngine();
+		// 2. 获得对应 Service
+		TaskService taskService = processEngine.getTaskService();
+		// 3. 办理任务
+		// taskId 就是 jbmp4_task表 DBID 字段
+		taskService.completeTask("30002");
+	}
+
 }
